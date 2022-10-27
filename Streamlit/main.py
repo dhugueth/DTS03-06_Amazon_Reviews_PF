3e8a30a6d2b0be7bae6baea8a4060c67b1db2234
Jean Fabra <jean.fabra@outlook.com>
2022-10-27 18:51:42 -0500

    Adding new scripts, ML models, updated README

---
+++ Streamlit/main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from navigation import landing, recomendacion_nlp_01, analisis_sentimientos
+
+# Streamlit Pages
+st.set_page_config(layout = 'wide')
+st.sidebar.image("https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/z3ahdkytzwi1jxlpazje",
+width=50)
+
+pages = {
+    'Main Page': landing.pageI,
+    'Recommendation system for users': recomendacion_nlp_01.pageII,
+    'Sentiment Analysis': analisis_sentimientos.pageIII
+}
+
+selected_pages = st.sidebar.radio("Navigation", pages.keys())
+pages[selected_pages]()
