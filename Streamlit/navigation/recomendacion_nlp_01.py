2ab366276322886d01a21eed81a33d5668c36275
Tomi <42160024+AstradaT@users.noreply.github.com>
2022-10-28 15:09:05 -0300

    Fix search in amazon

--- Streamlit/navigation/recomendacion_nlp_01.py
+++ Streamlit/navigation/recomendacion_nlp_01.py
@@ -7,13 +7,6 @@ warnings.filterwarnings('ignore')
 from functions import paginator
 
 
-def nav_to(url):
-    nav_script = """
-        <meta http-equiv="refresh" content="0; url='%s'">
-    """ % (url)
-    st.write(nav_script, unsafe_allow_html=True)
-
-
 def pageII():
     columns = st.columns((8,2,2))
     columns[1].image("img/skaivuinsightslogo.png", width = 350)
@@ -62,15 +55,13 @@ def pageII():
         for i in range(5):
             title1 = df_products['title'].where(df_products['asinID'] == products[i]).dropna().reset_index(drop=True)[0]
             col1.subheader(title1)
-            col1.write(f"ID: {products[i]}")
-            if col1.button("Search in Amazon↗️", key=f'amazonSearch{i}'):
-                nav_to(f'https://www.amazon.com/s?k={products[i]}')
+            url1 = 'https://www.amazon.com/s?k=' + products[i]
+            col1.markdown(f"ID: {products[i]} - [Search in Amazon]({url1})")
             image1 = df_products['imUrl'].where(df_products['asinID'] == products[i]).dropna().reset_index(drop=True)[0]
             col1.image(image1, width=110)
             title2 = df_products['title'].where(df_products['asinID'] == products[i+5]).dropna().reset_index(drop=True)[0]
             col2.subheader(title2)
-            col2.write(f"ID: {products[i+5]}")
-            if col2.button("Search in Amazon↗️", key=f'amazonSearch{i+5}'):
-                nav_to(f'https://www.amazon.com/s?k={products[i+5]}')
+            url2 = 'https://www.amazon.com/s?k=' + products[i+5]
+            col2.markdown(f"ID: {products[i+5]} - [Search in Amazon]({url2})")
             image2 = image1 = df_products['imUrl'].where(df_products['asinID'] == products[i+5]).dropna().reset_index(drop=True)[0]
             col2.image(image2, width=110)
