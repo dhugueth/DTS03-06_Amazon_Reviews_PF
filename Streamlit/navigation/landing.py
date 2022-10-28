c27841bb2f3d7fc3a8f39f43edb42d4a204fc3d2
Tomi <42160024+AstradaT@users.noreply.github.com>
2022-10-28 13:49:38 -0300

    Fix image path

--- Streamlit/navigation/landing.py
+++ Streamlit/navigation/landing.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def pageI():
     columns = st.columns((8,2,2))
-    columns[1].image("img\skaivuinsightslogo.png", width = 350)
+    columns[1].image("img/skaivuinsightslogo.png", width = 350)
     columns[0].title("🤖 Amazon Reviews: Machine Learning Models!")
 
     st.markdown("""### Welcome👋
