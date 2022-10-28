5e7ffa0973b28e5887be576c8c9d0a2a1fd77285
AstradaT <tomi_cuervocasla@hotmail.com>
2022-10-28 03:29:41 -0300

    Add recommendation system to streamlit app

--- Streamlit/navigation/recomendacion_nlp_01.py
+++ Streamlit/navigation/recomendacion_nlp_01.py
@@ -4,8 +4,61 @@ import requests
 import streamlit as st
 import pandas as pd
 warnings.filterwarnings('ignore')
+from functions import paginator
 
 def pageII():
     columns = st.columns((8,2,2))
-    columns[1].image("../img/skaivuinsightslogo.png", width = 350)
+    columns[1].image("img\skaivuinsightslogo.png", width = 350)
     columns[0].title("🤖 Recommendation System")
+
+    category = st.sidebar.selectbox("Category", (
+    "Amazon_Instant_Video", "Beauty", "Books", "CDs_Vinyls", "Cellphones_Accessories", 
+    "Clothing_Shoes_Jewelry", "Electronics", "Health_And_Personal", "Home_And_Kitchen", 
+    "Movies_And_TV", "Pet_Supplies", "Sports_And_Outdoors"
+    ))
+
+    df = pd.read_csv(f"Streamlit/datasets/results/{category}_results.csv")
+    df_products = pd.read_csv(f'Streamlit/datasets/products/{category}_products.csv')
+
+    df_recommendations = df[['user_index', 'recommended_products', 'recommend_top_items', 'reviewerID']].drop_duplicates()
+    df_recommendations.set_index(keys='user_index', drop=True, inplace=True)
+
+    # Ingresamos el 'user_index' y el 'num_recommendations' para el usuario
+    users = df_recommendations.index
+
+    user = st.sidebar.selectbox(
+        'User', users)
+
+    reviews = df[['asinID', 'title', 'reviewText', 'overall', 'imUrl']].where(df['user_index'] == user).dropna().reset_index(drop=True)
+
+    reviews_count = len(reviews)
+
+    st.subheader(f"User {user} reviewed {reviews_count} products from {category}")
+
+    pages = reviews['asinID'].to_list()
+
+    with st.expander(label="Show reviews"):
+        for i, page in paginator("Select a review page", pages, on_sidebar=False):
+            col1, col2 = st.columns([1,1])
+            col1.write('%s. **%s**' % (i, reviews.iloc[i]['title']))
+            col1.image(reviews.iloc[i]['imUrl'], width=130)
+            col2.metric("Rating", reviews.iloc[i]['overall'])
+            col2.write(f"Review: {reviews.iloc[i]['reviewText'][:200]}...")
+
+    st.subheader(f"This user was recommended these products:")
+
+    products = re.findall(r"(?<=\['| ')(.*?)(?=',|'\])", df_recommendations.iloc[user]['recommended_products'])
+
+    with st.expander(label="Show recommendations"):
+        col1, col2 = st.columns(2)
+        for i in range(5):
+            title1 = df_products['title'].where(df_products['asinID'] == products[i]).dropna().reset_index(drop=True)[0]
+            col1.subheader(title1)
+            col1.write(f"ID: {products[i]}")
+            image1 = df_products['imUrl'].where(df_products['asinID'] == products[i]).dropna().reset_index(drop=True)[0]
+            col1.image(image1, width=110)
+            title2 = df_products['title'].where(df_products['asinID'] == products[i+5]).dropna().reset_index(drop=True)[0]
+            col2.subheader(title2)
+            col2.write(f"ID: {products[i+5]}")
+            image2 = image1 = df_products['imUrl'].where(df_products['asinID'] == products[i+5]).dropna().reset_index(drop=True)[0]
+            col2.image(image2, width=110)
