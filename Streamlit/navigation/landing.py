0110caec11708f35397bd2c565219b390f2b5698
Tomi <42160024+AstradaT@users.noreply.github.com>
2022-10-28 14:00:20 -0300

    Update landing.py

--- Streamlit/navigation/landing.py
+++ Streamlit/navigation/landing.py
@@ -5,7 +5,8 @@ def pageI():
     columns[1].image("img/skaivuinsightslogo.png", width = 350)
     columns[0].title("🤖 Amazon Reviews: Machine Learning Models!")
 
-    st.markdown("""### Welcome👋
+    st.markdown("""
+        ### Welcome👋
         This application presents the following features:
         #### 1. Recommendation system of users who have made purchases and reviews. From a list of predetermined users and a list of categories, the desired number of products will be recommended.
         #### 2. Sentiment analysis for texts. You will be able to write a text and the general feeling will be determined from the words contained in it. 
@@ -17,4 +18,5 @@ def pageI():
         ###### [Tomás Astrada](https://www.linkedin.com/in/tom%C3%A1s-astrada-370b73171/) - Data Engineer
         ###### [Jean Fabra](https://www.linkedin.com/in/jeanfabra/) - Data Engineer 
         ###### [Jorge Fonseca](https://www.linkedin.com/in/jorge-fonseca-alba-83433b117/) - Data Scientist 
-        ###### [Daniela Hugueth](https://www.linkedin.com/in/dhugueth/) - Data Analyst""", unsafe_allow_html=True)
+        ###### [Daniela Hugueth](https://www.linkedin.com/in/dhugueth/) - Data Analyst
+        """, unsafe_allow_html=True)
